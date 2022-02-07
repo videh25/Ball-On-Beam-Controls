b16554532777e90089a7320ce2009e1fb40c39cd
Videh Patel <videh.p@iitgn.ac.in>
2022-02-07 19:42:43 +0530

    Added delay speed functionality

--- include/ir_sensor_interface.h
+++ include/ir_sensor_interface.h
@@ -8,8 +8,11 @@ class IR_Sensor_Interface
     // Methods and Function of the IR Sensor Interface
     private:
         int IR_Pin_;
-        float distance_values_[5];
+        float voltage_values_[5];
         double time_values_[5];
+        unsigned int speed_counter;
+        float curr_return_speed;
+        float last_dist_var_4speed;
 
     public:
         void get_state(float &dist_var, float &speed_var);
--- src/ir_sensor_interface.cpp
+++ src/ir_sensor_interface.cpp
@@ -4,6 +4,8 @@
 IR_Sensor_Interface::IR_Sensor_Interface(int irPin):
     IR_Pin_(irPin){
     // nothing here
+    speed_counter = 0;
+    last_dist_var_4speed = 0;
 }
 
 void IR_Sensor_Interface::IR_sensor_setup_code(){
@@ -15,22 +17,34 @@ void IR_Sensor_Interface::IR_sensor_setup_code(){
 
 void IR_Sensor_Interface::update_values(){
     for (int i = 0; i < 5; i++){
-        time_values_[i] = ((double)micros());
-        distance_values_[i] = 27.728 * pow(map(analogRead(IR_Pin_), 0, 1023, 0, 5000)/1000.0, -1.2045);
+        // time_values_[i] = ((double)micros());
+        voltage_values_[i] = analogRead(IR_Pin_);
     }
 }
 
 void IR_Sensor_Interface::get_state(float &dist_var, float &speed_var){
     update_values();
-    //float sum = 0; 
-    // for (int i = 0; i < 5; i++){
-    //     sum += distance_values_[i];
-    //     // time_values_[i] = (time_values_[i] - t0)/1E6;
-    // }
-    dist_var=(distance_values_[2]+distance_values_[3]+distance_values_[4])/3 ;
-    speed_var=(distance_values_[4]-distance_values_[1])/(3*(1));
-    if (abs(speed_var)<0.01){speed_var=0;} //perform the derivative of the error and add a dead band
-    speed_var=constrain(speed_var,-5,5); //limit the max size of the derivative of error to reduce high freq
+    float sum = 0; 
+    for (int i = 0; i < 5; i++){
+        sum += voltage_values_[i];
+        // time_values_[i] = (time_values_[i] - t0)/1E6;
+    }
+
+    dist_var = 27.728*pow(map(sum/5., 0, 1023, 0, 5000)/1000.0, -1.2045);
+
+    if (speed_counter > 10){
+        curr_return_speed = dist_var - last_dist_var_4speed;
+        if (abs(curr_return_speed)<0.1){curr_return_speed=0;}
+        curr_return_speed = constrain(curr_return_speed,-5,5);
+
+        last_dist_var_4speed = dist_var;
+        speed_counter = 0;
+    }else{
+        speed_counter++;
+    }
+
+    speed_var = curr_return_speed;
+     //limit the max size of the derivative of error to reduce high freq
     // float dist1 = sum/3;
 
     // update_values();
